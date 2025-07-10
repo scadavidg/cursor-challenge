c606b029514dc921955328968367f45c9394d0f8
santiago.cadavid <sacagu98@gmail.com>
2025-07-10 10:31:05 -0500

    deployment fix

--- src/app/api-docs/page.tsx
+++ src/app/api-docs/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
+
+const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
 export default function ApiDocs() {
   const [spec, setSpec] = useState(null);
